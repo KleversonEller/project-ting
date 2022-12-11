fb2bab9a9d77049d5d8cfb49e0f556ae5b1bd232
Kleverson Eller <kleverson.a16@gmail.com>
2022-12-11 11:36:55 -0300

    feat: :sparkles: Implementando funçoes (file_management.py)

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('txt'):
+        sys.stderr.write('Formato inválido\n')
+    try:
+        with open(path_file, 'r', encoding='utf8') as file:
+            content = file.read().splitlines()
+            return content
+    except IOError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
