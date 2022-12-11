2199eccd2b5074436a39b826d3e5da3c7f86d181
Kleverson Eller <kleverson.a16@gmail.com>
2022-12-11 11:38:26 -0300

    feat: :sparkles: Implementando funçoes (word_search.py)

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,29 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+
+    result = search_by_word(word, instance)
+
+    for dict in result:
+        for occurrence in dict['ocorrencias']:
+            del occurrence['conteudo']
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for dict in instance:
+        occurrences = []
+        for index, line in enumerate(dict['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                occurrences.append({
+                    'linha': index + 1,
+                    'conteudo': line
+                })
+
+        if len(occurrences):
+            data = {
+                'palavra': word,
+                'arquivo': dict['nome_do_arquivo'],
+                'ocorrencias': occurrences
+            }
+            result.append(data)
+    return result
