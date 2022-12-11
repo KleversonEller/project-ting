73771705a49f9788617ac62f2a7b4cf05b85a81b
Kleverson Eller <kleverson.a16@gmail.com>
2022-12-11 11:37:57 -0300

    feat: :sparkles: Implementando funçoes (file_process.py)

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,40 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
 
 
-def remove(instance):
-    """Aqui irá sua implementação"""
+def process(path_file: str, instance: Queue):
+    for i in range(len(instance)):
+        searched_item = instance.search(i)
+        if searched_item['nome_do_arquivo'] == path_file:
+            return None
+
+    txt_import = txt_importer(path_file)
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_import),
+        "linhas_do_arquivo": txt_import
+    }
+
+    instance.enqueue(new_dict)
+    print(new_dict)
+    sys.stdout.write(str(new_dict))
 
 
-def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+def remove(instance: Queue):
+    if not len(instance):
+        sys.stdout.write('Não há elementos\n')
+    else:
+        deleted_item = instance.dequeue()
+        sys.stdout.write(
+            f'Arquivo {deleted_item["nome_do_arquivo"]} removido com sucesso\n'
+            )
+
+
+def file_metadata(instance: Queue, position: int):
+    try:
+        result = instance.search(position)
+        sys.stdout.write(str(result))
+    except IndexError:
+        sys.stderr.write('Posição inválida\n')
